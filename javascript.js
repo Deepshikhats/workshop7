2652d4e5ec168b4428d67d4655acc692b465d5c3
unknown <deepshikha.mec@gmail.com>
2021-09-06 11:31:33 +0530

    changes

--- javascript.js
+++ javascript.js
@@ -1,20 +1,28 @@
-var description = document.getElementById("button_click_description");
+
 var count = 0;
-function showhidelist(){
+function showhidelist(button_id){
     count = count + 1;
+    var btn = document.getElementById(button_id);
+    var description = document.getElementById(button_id+"_description");
     if (count%2 != 0){
         description.style.display = "flex";
+        btn.style.background = "#FEF5E5";
+        btn.style.color = "#EB9F24";
+        btn.style.borderBottom = "#EB9F24 2px solid";
     }
     else{
         description.style.display = "none";
+        btn.style.background = "white";
+        btn.style.color = "#626262";
+        btn.style.borderBottom = " #626262 2px solid";
     }
     
 }
 var legal = document.getElementById("legal");
 function dispay(){
-    legal.innerHTML = "Legal";
-
+  legal.innerHTML = "Legal";
 }
+
 function changeImage(img_id) {
     var image = document.getElementById(img_id);
     if (image.src.match("Assets/love_grey.png")) {
